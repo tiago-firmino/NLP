6b92fa2bbb67ef34ed2c7c13b09ce19d4b2838db
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-10-10 17:43:33 +0100

    Update project.py

--- project.py
+++ project.py
@@ -10,6 +10,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+import pandas as pd
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
@@ -28,4 +29,9 @@ for token in lema:
     if token not in stop_words and token not in punc:
         new_tokens.append(token)
 
-print(new_tokens)
+#Dividir os dados em treino e validação
+
+data = pd.read_csv('train.txt', sep='\t', header=None, names=['Title', 'Country', 'Genre', 'Director', 'Plot'])
+genre_list = data['Genre'].tolist()
+
+print(genre_list)
