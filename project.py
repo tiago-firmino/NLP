7084b03dccf924170d9923327289d30762dc3438
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-10-10 18:07:23 +0100

    Update project.py

--- project.py
+++ project.py
@@ -14,24 +14,26 @@ import pandas as pd
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-with open('train.txt', 'r', encoding='utf-8') as file:
-    content = file.read()
-    content = content.lower()
+text = pd.read_csv('train.txt', sep='\t', header=None, names=['Title', 'Country', 'Genre', 'Director', 'Plot'])
+#list of plot strings
+plot = text['Plot'].tolist()
     
 stop_words = stopwords.words('english')
 punc = string.punctuation
 lemmatizer = WordNetLemmatizer()
 
 new_tokens=[]
-tokens=nltk.word_tokenize(content)
+tokens = [nltk.word_tokenize(p) for p in plot]
+tokens = [word.lower() for sublist in tokens for word in sublist]
 lema = [lemmatizer.lemmatize(word) for word in tokens]
 for token in lema:
     if token not in stop_words and token not in punc:
         new_tokens.append(token)
+
+print(new_tokens)
 
 #Dividir os dados em treino e validação
 
 data = pd.read_csv('train.txt', sep='\t', header=None, names=['Title', 'Country', 'Genre', 'Director', 'Plot'])
 genre_list = data['Genre'].tolist()
 
-print(genre_list)
