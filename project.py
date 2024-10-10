0d6ce63311ba7cc57649cecc32c4f204588db0cf
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-10-10 17:20:18 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,4 +1,3 @@
-#clean the text of movie plots and prepare it for the model
 import sys
 import io
 import re
@@ -13,8 +12,8 @@ import pandas as pd
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-with open('train.txt', 'r', encoding='utf-8') as arquivo:
-    content = arquivo.read()
+with open('train.txt', 'r', encoding='utf-8') as file:
+    content = file.read()
     content = content.lower()
     
 stop_words = stopwords.words('english')
