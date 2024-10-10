246b5c799d3b445967c29fad446113233cf4cfc6
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-10-10 17:24:34 +0100

    Update project.py

--- project.py
+++ project.py
@@ -5,10 +5,11 @@ import nltk
 import string
 nltk.download('punkt')       
 nltk.download('stopwords')
-
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import pandas as pd
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
@@ -18,9 +19,11 @@ with open('train.txt', 'r', encoding='utf-8') as file:
     
 stop_words = stopwords.words('english')
 punc = string.punctuation
+lemmatizer = WordNetLemmatizer()
 
 new_tokens=[]
 tokens=nltk.word_tokenize(content)
+lemm = [lemmatizer.lemmatize(word) for word in tokens]
 for token in tokens:
     if token not in stop_words and token not in punc:
         new_tokens.append(token)
